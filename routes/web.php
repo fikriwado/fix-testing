3e6270e3de8909ca8217fd0c52bd5cc4d07a094f
fikriwado <mfikri.khoirurrizal.com>
2025-01-10 17:17:01 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('iam', function () {
     return 'Hello from the iam package';
 });
+
+Route::get('fixwad', function () {
+    return 'Hello from the iam package';
+});
